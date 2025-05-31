fd6a5fac9578ee6087cb91ecd449e4c7feed4a6a
Lucas Bueno <lucasbueno2004@hotmail.com>
2025-05-30 21:09:30 -0300

    adding cadastro aluno

--- run.py
+++ run.py
@@ -3,10 +3,11 @@ from src.main.process_handler import ProcessHandler
 from src.view.app_views.Windown import Windown
 from src.view.app_views.TelaLogin import Telalogin
 from src.view.app_views.AdminFrame import AdminFrame
+from src.view.app_views.CadAlunoFrame import CadAlunoFrame
 
 if __name__ == "__main__":
 
     j = Windown()
-    t = AdminFrame(master=j)
+    t = CadAlunoFrame(master=j)
     t.place(x = 0, y = 0)
     j.mainloop()
---
+++ src/view/app_views/CadAlunoFrame.py
@@ -0,0 +1,85 @@
+import customtkinter as ctk
+import tkinter as tk
+from src.view.app_views.app_view_settings import settings
+from PIL import Image
+from typing import Callable
+
+class CadAlunoFrame(ctk.CTkFrame):
+
+    def __init__(self, master, width = settings['MainWidth'], height =settings['MainHeight'], corner_radius = None, border_width = None, bg_color = "transparent", fg_color = None, border_color = None, background_corner_colors = None, overwrite_preferred_drawing_method = None, **kwargs):
+        super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
+
+        self.bg_img = ctk.CTkImage(dark_image=Image.open("src/assets/bgimage.jpeg"), light_image=Image.open("src/assets/bgimage.jpeg"), size=(settings['MainWidth'],settings['MainHeight']))
+
+        self.background_label = ctk.CTkLabel(
+            master=self, 
+            width=settings['MainWidth'],
+            height=settings['MainHeight'],
+            text=None,
+            image=self.bg_img
+        )
+        self.background_label.place(x = 0, y = 0)
+
+
+        self.novo_aluno_label = ctk.CTkLabel(
+            master=self.background_label,
+            text='Cadastro de Aluno:',
+            text_color="#FFFFFF",
+            font=settings['HeaderFont'],
+            width=300, 
+            height=50, 
+            bg_color='#003E69',
+            fg_color="#003E69"
+        )
+
+        self.novo_aluno_label.place(x = 362, y = 50)
+
+        self.email_entry = ctk.CTkEntry(
+            master=self.background_label, 
+            width=300, 
+            height=50, 
+            placeholder_text="Email",
+            corner_radius=10, 
+            fg_color="#CFCECD",
+            bg_color="#003E69",
+            text_color="#000000",
+            font=settings['ParagraphFont']
+        )
+        self.email_entry.place(x = 362, y = 150)
+
+        self.password_entry = ctk.CTkEntry(
+            master=self.background_label,
+            width=300, 
+            height=50, 
+            placeholder_text="Senha",
+            corner_radius=10, 
+            fg_color="#CFCECD",
+            bg_color="#003E69",
+            text_color="#000000",
+            font=settings['ParagraphFont']
+        )
+
+        self.password_entry.place( x = 362, y = 250)
+
+        self.password_confirm_entry = ctk.CTkEntry(
+            master = self.background_label,
+            width=300,
+            height=50, 
+            placeholder_text="Confirmar Senha",
+            corner_radius=10, 
+            fg_color="#CFCECD",
+            bg_color="#003E69",
+            text_color="#000000",
+            font=settings['ParagraphFont']
+        )
+
+        self.password_confirm_entry.place(x = 362, y = 350)
+
+        self.send_button = ctk.CTkButton(master=self.background_label,
+                                         width=300, 
+                                         height=100, 
+                                         text="Salvar", 
+                                         font=settings['ParagraphFont'], bg_color="#003E69", fg_color= "#FF6F00", hover_color="#F12754",
+                                         corner_radius=10)
+        
+        self.send_button.place(x =362, y = 450 )
